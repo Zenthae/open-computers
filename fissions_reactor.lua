6538196e08adc41c8348ae10f95550c6f14c4374
Zenthae <zenthae@gmail.com>
2020-04-15 00:45:10 +0200

    move file to src/

--- fissions_reactor.lua
+++
@@ -1,4 +0,0 @@
---Nuclearcraft Fusion Reactor Controller
-
-local component = require("component")
-local reactor = component.nc_fission_reactor
---
+++ src/fissions_reactor.lua
@@ -0,0 +1,4 @@
+--Nuclearcraft Fusion Reactor Controller
+
+local component = require("component")
+local reactor = component.nc_fission_reactor
